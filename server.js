874010da8fe0cc5ec349fdb66671c099bdf15b5b
Katherine Yoguez <katherinecarlotayoguez@gmail.com>
2021-07-31 17:57:09 -0400

    updated server.js

--- server.js
+++ server.js
@@ -5,3 +5,24 @@ const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+});
+
+app.use(require("./routes/apiRoutes"));
+app.use(require("./routes/htmlRoutes"));
+
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
