82188502fd2776d98c75375bd557995fc73fa73e
Katherine Yoguez <katherinecarlotayoguez@gmail.com>
2021-08-02 13:20:47 -0400

    updated workout.js

--- models/workout.js
+++ models/workout.js
@@ -11,13 +11,27 @@ const workoutSchema = new Schema({
 
     exercises: [
         {
-            type: String,
-            name: String,
-            duration: Number,
-            weight: Number,
-            reps: Number,
-            sets: Number
-
+            type: {
+                type: String
+            } ,
+            name: {
+                type: String
+            } ,
+            duration: {
+                type: Number
+            },
+            weight: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            sets:{
+                type: Number
+            } ,
+            distance: {
+                type: Number
+            }
         }]
 });
 
--- server.js
+++ server.js
@@ -19,7 +19,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useFindAndModify: false
 });
 
-app.use(require("./routes/apiRoutes"));
+require("./routes/apiRoutes")(app);
+// require("./routes/htmlRoutes")(app);
 app.use(require("./routes/htmlRoutes"));
 
 
